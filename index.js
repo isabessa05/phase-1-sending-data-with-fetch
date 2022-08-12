a9a31d030af4c0059023e32abce21f07a56242dd
+ <+>
2022-08-12 15:57:56 -0400

    commit tree 46cb6e8e4ea90aab34a946f0dd58ca8fc687a187

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+
+const { assertLiteral } = require("babel-types");
+const { json } = require("body-parser")
+const { userInfo } = require("os")
+
+function submitData (name, email){
+
+    return fetch('http://localhost:3000/users', {
+
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify({
+        name,
+        email,
+    })
+})
+.then(function (response) {
+    return response.json();
+})
+
+.then (function (object) {
+    document.body.innerHTML = object ["id"]
+})
+
+.catch(function (error) {
+    alert ("Something is wrong!");
+    document.body.innerHTML = error.message
+})
+}
